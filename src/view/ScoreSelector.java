c6ae714762019ebc30fdc1b530fb4b15c5ec2e65
roche <gox@gox-laptop.(none)>
2011-05-19 13:10:49 +0200

    Classes pour gérer le classement

---
+++ src/view/ScoreSelector.java
@@ -0,0 +1,100 @@
+package view;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Font;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.TrueTypeFont;
+import org.newdawn.slick.UnicodeFont;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.transition.FadeInTransition;
+import org.newdawn.slick.state.transition.FadeOutTransition;
+import org.newdawn.slick.state.transition.Transition;
+
+	public class ScoreSelector extends BasicGameState {
+		private StateBasedGame game;
+		private GameContainer container;
+		private Integer selected = 0;
+
+		public void init(GameContainer gc, StateBasedGame sbg)
+				throws SlickException {
+			this.container = gc;
+			this.game = sbg;
+		}
+
+		public void update(GameContainer arg0, StateBasedGame arg1, int arg2)
+				throws SlickException {
+		}
+
+		public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
+				throws SlickException {
+			int i = 0;	
+	    	g.setBackground(Color.black);
+	    	g.setFont(new TrueTypeFont(new java.awt.Font(java.awt.Font.SANS_SERIF, java.awt.Font.BOLD, 50), true));
+			g.setColor(new Color(255, 215, 0));
+			for (int y = 0; y < 5; ++y) {
+				for (int x = 0; x < 7; ++x) {
+					if (y * 7 + x == selected) {
+						g.setColor(new Color(255, 0, 0));
+					}
+					
+					else {
+						g.setColor(new Color(255, 215, 0));
+					}
+					
+					g.fillRect(15 + x * 135, 15 + y * 135, 120, 120);
+
+					g.setColor(new Color(0, 0, 0));
+					g.drawString("" + (y * 7 + x + 1), 15 + x * 135, 15 + y * 135);
+				}
+			}
+		}
+
+		public void keyPressed(int key, char c) {
+
+			 if (key == Input.KEY_ENTER || key == Input.KEY_SPACE) {
+				try {
+					((Scores) game.getState(8)).setLevel(selected + 1);
+					game.getState(8).init(container, game);
+				} catch (SlickException e) {
+					e.printStackTrace();
+				}
+				game.enterState(8, new FadeOutTransition(), new FadeInTransition());
+			} else {
+				int d = 0;
+				switch (key) {
+					case Input.KEY_ESCAPE:
+						try {
+							game.getState(10).init(container, game);
+						} catch (SlickException e) {
+							e.printStackTrace();
+						}
+						game.enterState(10, new FadeOutTransition(), new FadeInTransition());
+				
+	    			case Input.KEY_UP:
+	    				d = -7;
+	    				break;
+	    			case Input.KEY_DOWN:
+	    				d = 7;
+	    				break;
+	    			case Input.KEY_LEFT:
+	    				d = -1;
+	    				break;
+	    			case Input.KEY_RIGHT:
+	    				d = 1;
+	    				break;		
+				}
+				if (selected + d >= 0 && selected + d < 7 * 5) {
+					selected += d;
+				}
+			}
+		}
+
+		public int getID() {
+			return 7;
+		}
+}
+
---
+++ src/view/Scores.java
@@ -0,0 +1,120 @@
+package view;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+import model.database.Database;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.TrueTypeFont;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.transition.FadeInTransition;
+import org.newdawn.slick.state.transition.FadeOutTransition;
+
+public class Scores extends BasicGameState { 
+
+	private Database db;
+	private LinkedHashMap<String, String> ladder;
+	private int levelSelected;
+	private GameContainer container;
+	private StateBasedGame game;
+	
+	@Override
+	public int getID() {
+		return 8;
+	}
+
+	@Override
+	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+		
+		this.container = gc;
+		this.game = sbg;
+		ladder = new LinkedHashMap<String, String>();
+		
+		db = new Database();
+
+		ResultSet rs = null;
+		try {
+			rs = db.getClassement(levelSelected);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			while(rs.next()){
+				String name = rs.getString(1);
+				String score = rs.getString(2);
+				ladder.put(name, score);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			db.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void render(GameContainer arg0, StateBasedGame arg1, Graphics g)
+			throws SlickException {
+
+		g.setBackground(Color.blue);
+    	g.setColor(Color.white);
+    	g.setFont(new TrueTypeFont(new java.awt.Font(java.awt.Font.SANS_SERIF, java.awt.Font.BOLD, 40), true));
+    	g.drawString("Classement niveau "+ levelSelected, 200, 50);
+    	g.setFont(new TrueTypeFont(new java.awt.Font(java.awt.Font.SANS_SERIF, java.awt.Font.BOLD, 30), true));
+    	
+    	int x = 300;
+    	int y = 200;
+    	int rank = 1;
+    	
+    	for (String name : ladder.keySet()) {
+    		if(rank != 10){
+	    		 g.drawString(""+rank, x-65, y);
+	    		 g.drawString(name, x, y);
+	    		 g.drawString(ladder.get(name), x+300, y);
+	    		 y = y + 50;
+	    		 rank++;
+    		}
+    		else
+    			break;
+    	}
+	}
+
+	public void setLevel(int l){
+		levelSelected = l;
+	}
+	
+	@Override
+	public void update(GameContainer arg0, StateBasedGame arg1, int arg2)
+			throws SlickException {
+	}
+	
+	public void keyPressed(int key, char c) {
+		if (key == Input.KEY_ENTER || key == Input.KEY_SPACE) {
+			try {
+				game.getState(7).init(container, game);
+			} catch (SlickException e) {
+				e.printStackTrace();
+			}
+			game.enterState(7, new FadeOutTransition(), new FadeInTransition());
+		}
+		else if(key == Input.KEY_ESCAPE){
+			try {
+				game.getState(7).init(container, game);
+			} catch (SlickException e) {
+				e.printStackTrace();
+			}
+			game.enterState(7, new FadeOutTransition(), new FadeInTransition());
+		}
+			
+	}
+}
